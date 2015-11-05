d8b9f5cf4c7b8ddcd462d95926a866aef2058281
Chua Wei Kuan <khankuan@gmail.com>
2015-11-05 01:14:05 -0800

    Allow children to be wrapped inside waypoint. This makes it easier to use waypoint without worrying about the alignment of the dummy span.

--- src/waypoint.jsx
+++ src/waypoint.jsx
@@ -197,7 +197,7 @@ const Waypoint = React.createClass({
   render() {
     // We need an element that we can locate in the DOM to determine where it is
     // rendered relative to the top of its context.
-    return <span style={{fontSize: 0}} />;
+    return this.props.children || <span style={{fontSize: 0}} />;
   }
 });
 
