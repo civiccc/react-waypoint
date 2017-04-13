091d0a10aa0d893f72bfc13fe070dea7aec06a5f
Henric Trotzig <henric.trotzig@brigade.com>
2017-04-13 09:18:01 +0200

    Explain why .importjs.js is there
    I'm so blinded by always adding this config file to all my projects, so
    it never crossed my mind to explain why it was there. Adding a comment
    at the top with a link to the import-js github repo should make things
    clearer.
    
    Thanks to @jmeas for pointing this out in code review.

--- .importjs.js
+++ .importjs.js
@@ -1,3 +1,5 @@
+// This config file controls how import-js behaves
+// https://github.com/Galooshi/import-js
 module.exports = {
   importDevDependencies: true,
   environments: ['node', 'browser'],
