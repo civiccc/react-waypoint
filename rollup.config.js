c7df56c86d25719780b99c9f158009e099f16107
Henric Trotzig <henric.trotzig@brigade.com>
2017-10-05 17:06:50 +0200

    Fix missing reference to file in rollup.config.js
    Now that the target is gone from package.json, we have to specify the
    output file manually.

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ export default [
     ],
     output: [
       { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { file: 'build/index.mjs', format: 'es' }
     ],
     plugins: [
       babel({
