0cedcd4201baf525b75d51b8da04223e020f59cd
Rouven Weßling <rouven@contentful.com>
2019-01-23 22:06:01 +0100

    Switch eslint to eslint-config-airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-  'extends': 'eslint-config-brigade/react',
+  'extends': 'eslint-config-airbnb',
   'parserOptions': {
-    'ecmaVersion': 6,
-    'sourceType': 'module'
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+  },
+  'rules': {
+    'no-restricted-globals': 'off',
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off'
+  },
+  'env': {
+    'browser': true,
   },
 };
