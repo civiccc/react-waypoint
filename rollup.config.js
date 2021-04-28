8bbd191e37afcf76b867edd0a736b2f1a075bb5c
Joe Lencioni <joe.lencioni@airbnb.com>
2021-04-28 11:12:03 -0500

    Use external @babel/runtime helpers
    Fixes #302
    
    This is similar to #341, except it avoids having to run babel on the
    rollup output, which allows us to leave the babel configuration in
    the .babelrc file.

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,40 @@
 import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
-export default [
-  {
+const depsSet = new Set([
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.devDependencies),
+]);
+
+/**
+ * @param {'es' | 'cjs'} format
+ */
+function makeBuild(format) {
+  return {
     input: 'src/waypoint.jsx',
-    external: [
-      ...Object.keys(pkg.dependencies),
-      ...Object.keys(pkg.peerDependencies),
-    ],
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-    ],
+
+    external: (id) => {
+      if (id.startsWith('.') || id.startsWith('/')) {
+        return false;
+      }
+
+      const packageName = id.startsWith('@')
+        ? id.split('/').slice(0, 2).join('/')
+        : id.split('/')[0];
+
+      return depsSet.has(packageName);
+    },
+
+    output: [{ file: format === 'es' ? pkg.module : pkg.main, format }],
+
     plugins: [
       babel({
-        babelHelpers: 'bundled',
+        babelHelpers: 'runtime',
+        envName: format,
         exclude: ['node_modules/**'],
       }),
     ],
-  },
-];
+  };
+}
+
+export default [makeBuild('es'), makeBuild('cjs')];
